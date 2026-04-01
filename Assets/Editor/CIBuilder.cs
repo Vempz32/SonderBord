2c047aaa42b9656ad9bb687d55e5b53bc5091b61
Tyler Jablonski <Tyjablonski@icloud.com>
2026-04-01 15:04:40 -0700

    Fixed things

---
+++ Assets/Editor/CIBuilder.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+using System;
+using UnityEditor.PackageManager;
+
+public class CIBuilder 
+{
+  [MenuItem("CI/BuildWebGL")]
+  public static void PerformWebGlBuild()
+  {
+    try
+    {
+      string[] scenes = EditorBuildSettings.scenes
+        .Where(s => s.enabled)
+        .Select(s => s.path)
+        .ToArray();
+
+      if (scenes.Length == 0)
+      {
+        Debug.LogError("No scenes are added to build");
+        return;
+      }
+
+      string buildPath = "Builds/WebGL";
+      if (!Directory.Exists(buildPath))
+      {
+        Directory.CreateDirectory(buildPath);
+        Debug.Log($"Created director at {buildPath}");
+      }
+
+      BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+      buildPlayerOptions.scenes = scenes;
+      buildPlayerOptions.locationPathName = buildPath;
+      buildPlayerOptions.target = BuildTarget.WebGL;
+      buildPlayerOptions.options = BuildOptions.None;
+
+      UnityEditor.Build.Reporting.BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+      UnityEditor.Build.Reporting.BuildSummary summary = report.summary;
+
+      if (summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded)
+      {
+        Debug.Log($"Build Succeeded. {summary.totalSize} bytes written");
+      }
+      else if (summary.result == UnityEditor.Build.Reporting.BuildResult.Failed)
+      {
+        Debug.Log($"Build Failed with {summary.totalErrors} errors");
+
+      }
+    }
+    catch
+    {
+      Debug.Log($"Critical Error build Failed");
+    }
+  }
+}
